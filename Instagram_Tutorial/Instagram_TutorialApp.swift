4c87c75dc765691423de1aad43cab150b4236d42
BumgeunSong <isp1195@gmail.com>
2023-12-29 22:51:28 +0900

    Initial Commit

---
+++ Instagram_Tutorial/Instagram_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_TutorialApp.swift
+//  Instagram_Tutorial
+//
+//  Created by Bumgeun Song on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
