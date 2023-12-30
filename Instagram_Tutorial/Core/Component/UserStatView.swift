3a7e790c2eec58a2bd1f3f61aee11adb0132a625
BumgeunSong <isp1195@gmail.com>
2023-12-30 21:44:17 +0900

    add new image for profile pic

---
+++ Instagram_Tutorial/Core/Component/UserStatView.swift
@@ -0,0 +1,18 @@
+//
+//  UserStatView.swift
+//  Instagram_Tutorial
+//
+//  Created by Bumgeun Song on 12/30/23.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserStatView()
+}
