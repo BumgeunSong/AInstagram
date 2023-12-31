d0b01b8e2ddfc5e352f2608c94dbced9bd94fed5
BumgeunSong <isp1195@gmail.com>
2023-12-31 18:52:54 +0900

    Feat: 하나의 피드당 들어갈 요소 레이아웃 잡기

--- Instagram_Tutorial/Core/Feed/FeedCell.swift
+++ Instagram_Tutorial/Core/Feed/FeedCell.swift
@@ -9,7 +9,74 @@ import SwiftUI
 
 struct FeedCell: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // Profile Pic + Username
+        VStack {
+            HStack {
+                Image(.eddyDesc)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 48, height: 48)
+                    .clipShape(Circle())
+                
+                Text("bumgeun.song")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+            }.padding(.horizontal)
+            
+            
+            // Post Image
+            Image(.london)
+                .resizable().scaledToFill()
+                .frame(height: 400)
+                .clipShape(Rectangle())
+            
+            // Action row
+            HStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                }).tint(.black)
+                Button(action: {}, label: {
+                    Image(systemName: "bubble.right")
+                        .imageScale(.large)
+                }).tint(.black)
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }).tint(.black)
+                
+                Spacer()
+            }.padding(.horizontal).padding(.top, 4)
+            
+            // likes
+            Text("1 likes")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)  // 왼쪽 정렬로 붙이고 싶을 때
+                .padding(.horizontal)
+                .padding(.vertical, 1)
+            
+            // Post Caption
+            HStack {
+                Text("bumgeun.song ")
+                    .fontWeight(.semibold) +
+                Text("This is London 🏴󠁧󠁢󠁥󠁮󠁧󠁿 It was real fun experience! ")
+            }
+            .font(.footnote)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal)
+            
+            // time
+            Text("5h ago")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)  // 왼쪽 정렬로 붙이고 싶을 때
+                .padding(.horizontal)
+                .padding(.vertical, 1)
+                .foregroundStyle(Color(.darkGray))
+        }
     }
 }
 
