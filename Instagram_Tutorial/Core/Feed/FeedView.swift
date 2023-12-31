4010bd439f275b5c989596a4aef34f817bb9c993
BumgeunSong <isp1195@gmail.com>
2023-12-31 19:03:45 +0900

    Feat: 피드 뷰에 네비게이션 바, 로고 설정

--- Instagram_Tutorial/Core/Feed/FeedView.swift
+++ Instagram_Tutorial/Core/Feed/FeedView.swift
@@ -19,6 +19,24 @@ struct FeedView: View {
             }
             .navigationTitle("Feed")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar(content: {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(.instagramTypoLogo)
+                            .resizable()
+                            .tint(.black)
+                            .frame(width: 84, height: 48)
+                    })
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "paperplane")
+                            .imageScale(.large)
+                            .tint(.black)
+                            .fontWeight(.semibold)
+                    })
+                }
+            })
         }
         
     }
