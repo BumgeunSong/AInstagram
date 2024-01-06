6a19e7869f3e212ccfce9823fbf5898f602ce9b8
BumgeunSong <isp1195@gmail.com>
2024-01-06 17:46:55 +0900

    feat: 포스트용 프롬프트를 담은 PostViewModel 생성

---
+++ Instagram_Tutorial/Core/UploadPost/UploadPostViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  UploadPostViewModel.swift
+//  Instagram_Tutorial
+//
+//  Created by Bumgeun Song on 1/6/24.
+//
+
+import Foundation
+
+@MainActor
+class UploadPostViewModel: ObservableObject {
+    
+    @Published var selectedPrompt: String?
+    @Published var recentPrompts: [String] = []
+    
+    func loadRecentPrompts() async {
+        self.recentPrompts = [
+            "Generate a stunning concept for an expansive fantasy world, complete with enchanted forests, towering castles, and mythical creatures, setting the stage for a grand adventure.",
+            "Generate a cartoon character in the style of classic Disney animation, with large, expressive eyes and whimsical features.",
+            "Generate poetic images of nature, blending landscapes and elements to convey the beauty and harmony of the natural world for nature-centric blogs/environmental causes.",
+            "Create stunning photos capturing the essence of a high-fashion runway show, featuring the latest couture."
+        ]
+    }
+}
