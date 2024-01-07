c754d6ab89025dd223a7dda2791078f972e492b7
BumgeunSong <isp1195@gmail.com>
2024-01-07 20:18:41 +0900

    feat: 프로필 뷰 상단 버튼 눌렀을 때 로그아웃 얼럿 띄우기

--- Instagram_Tutorial/Core/Profile/ProfileView.swift
+++ Instagram_Tutorial/Core/Profile/ProfileView.swift
@@ -12,6 +12,8 @@ struct ProfileView: View {
     let user: User
     let posts: [Post] = Post.mock
     
+    @State var isSignoutAlertPresented: Bool = false
+    
     var body: some View {
         NavigationStack {
             ScrollView {
@@ -22,10 +24,24 @@ struct ProfileView: View {
             .navigationBarTitleDisplayMode(.inline)
             .toolbar(content: {
                 ToolbarItem(placement: .topBarTrailing) {
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Button(action: {
+                        isSignoutAlertPresented = true
+                    }, label: {
                         Image(systemName: "line.3.horizontal")
                             .foregroundStyle(.black)
                     })
+                    .alert(
+                        "로그아웃 하시겠어요?",
+                        isPresented: $isSignoutAlertPresented,
+                        presenting: false
+                    ) { _ in
+                        Button("취소", role: .cancel, action: {})
+                        Button("로그아웃", action: {
+                            Task {
+                                try await AuthService.shared.signout()
+                            }
+                        })
+                    }
                 }
             })
         }
