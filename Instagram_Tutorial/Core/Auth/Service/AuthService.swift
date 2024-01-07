271b0e909fb1b76e4764174c879f857073742d1f
BumgeunSong <isp1195@gmail.com>
2024-01-07 15:06:24 +0900

    feat: 회원가입/인증 담당 서비스 생성

---
+++ Instagram_Tutorial/Core/Auth/Service/AuthService.swift
@@ -0,0 +1,18 @@
+//
+//  AuthService.swift
+//  Instagram_Tutorial
+//
+//  Created by Bumgeun Song on 1/7/24.
+//
+
+import Foundation
+
+class AuthService {
+    func login(withEmail email: String, password: String) async throws {
+        
+    }
+    
+    func createUser(email: String, password: String, userName: String) async throws {
+        
+    }
+}
