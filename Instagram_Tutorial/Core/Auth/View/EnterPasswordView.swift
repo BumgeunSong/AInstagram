4fe3c35b49ab7c0ff21740b2ff1038b603a848d8
BumgeunSong <isp1195@gmail.com>
2024-01-07 17:36:29 +0900

    feat: 회원가입 (2) 비밀번호 입력 뷰 추가

---
+++ Instagram_Tutorial/Core/Auth/View/EnterPasswordView.swift
@@ -0,0 +1,49 @@
+//
+//  EnterPasswordView.swift
+//  Instagram_Tutorial
+//
+//  Created by Bumgeun Song on 1/7/24.
+//
+
+import SwiftUI
+
+struct EnterPasswordView: View {
+    @State var password: String = ""
+    
+    var body: some View {
+        Text("비밀번호를 입력해주세요")
+            .font(.title)
+            .fontWeight(.bold)
+            .padding(.all)
+            
+        Text("앞으로 로그인할 때 사용하게 될 거예요.")
+            .font(.subheadline)
+            .padding(.all)
+        
+        TextField("비밀번호 입력", text: $password)
+            .textInputAutocapitalization(.none)
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(uiColor: .systemGray6))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding(.horizontal, 24)
+        
+        Button(action: {
+            print("Login")
+        }, label: {
+            Text("다음")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, maxHeight: 44)
+                .foregroundStyle(.white)
+                .background(Color(uiColor: .systemBlue))
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+                .padding(.horizontal, 24)
+        })
+        .padding(.vertical)
+    }
+}
+
+#Preview {
+    EnterPasswordView()
+}
