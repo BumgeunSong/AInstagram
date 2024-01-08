8264363f52d74e8d7b1d51f170f25d0e1757e79d
BumgeunSong <isp1195@gmail.com>
2024-01-08 11:26:24 +0900

    feat: 프로필 편집할 수 있는 뷰, 뷰모델 추가

---
+++ Instagram_Tutorial/Core/Profile/ProfileEditView.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileEditView.swift
+//  Instagram_Tutorial
+//
+//  Created by Bumgeun Song on 1/8/24.
+//
+
+import SwiftUI
+
+struct ProfileEditView: View {
+    var body: some View {
+        
+        VStack {
+            // 프로필 이미지
+            
+            // 풀네임
+            
+            // 사용자 이름
+            
+            // Bio
+            
+        }
+        
+        // 수정하기 CTA
+    }
+}
+
+#Preview {
+    ProfileEditView()
+}
---
+++ Instagram_Tutorial/Core/Profile/ProfileEditViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileEditViewModel.swift
+//  Instagram_Tutorial
+//
+//  Created by Bumgeun Song on 1/8/24.
+//
+
+import Foundation
+
+final class ProfileEditViewModel: ObservableObject {
+    
+}
