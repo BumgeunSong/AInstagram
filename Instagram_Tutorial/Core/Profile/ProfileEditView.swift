01d7aead7c4925f78c2dd5e5ac44b94a5aa00561
BumgeunSong <isp1195@gmail.com>
2024-01-08 11:30:56 +0900

    feat: 프로필 수정 뷰에 텍스트 필드, CTA 추가

--- Instagram_Tutorial/Core/Profile/ProfileEditView.swift
+++ Instagram_Tutorial/Core/Profile/ProfileEditView.swift
@@ -8,20 +8,43 @@
 import SwiftUI
 
 struct ProfileEditView: View {
+    @State var username: String = ""
+    @State var fullname: String = ""
+    @State var bio: String = ""
+    
     var body: some View {
         
         VStack {
             // 프로필 이미지
             
             // 풀네임
-            
+            TextField(text: $username, prompt: Text("Full Name")) {
+                Text("Full name")
+            }
             // 사용자 이름
+            TextField(text: $fullname, prompt: Text("User name")) {
+                Text("User name")
+            }
             
             // Bio
-            
-        }
+            TextField(text: $bio, prompt: Text("Bio")) {
+                Text("bio")
+            }
+        }.padding(.horizontal)
         
         // 수정하기 CTA
+        NavigationLink {
+            //
+        } label: {
+            Text("확인")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, maxHeight: 44)
+                .foregroundStyle(.white)
+                .background(Color(uiColor: .systemBlue))
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+                .padding(.horizontal, 24)
+        }.padding(.vertical)
     }
 }
 
