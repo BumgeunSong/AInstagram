da7bd40269f35e1f59310a1bf5ec539a168f113d
BumgeunSong <isp1195@gmail.com>
2024-01-13 14:00:37 +0900

    feat: 프로필 사진 picker 추가

--- Instagram_Tutorial/Core/Profile/EditProfileView.swift
+++ Instagram_Tutorial/Core/Profile/EditProfileView.swift
@@ -6,9 +6,11 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct EditProfileView: View {
     @Environment(\.dismiss) var dismiss
+    @State private var selectedImage: PhotosPickerItem?
     
     var body: some View {
         VStack {
@@ -42,6 +44,21 @@ struct EditProfileView: View {
         }
         // Profile
         
+        PhotosPicker(selection: $selectedImage) {
+            VStack {
+                Image(systemName: "person")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .foregroundStyle(.white)
+                    .background(.gray)
+                    .clipShape(Circle())
+                
+                Text("Edit Profile Picture")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+            }
+        }
+        .padding(.vertical, 8)
         
     }
 }
