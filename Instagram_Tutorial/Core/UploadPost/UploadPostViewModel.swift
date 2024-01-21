f39551be488871bbbe587e4e7519b8ec1add1d30
BumgeunSong <isp1195@gmail.com>
2024-01-21 13:48:49 +0900

    feat: 추천 프롬프트 추가

--- Instagram_Tutorial/Core/UploadPost/UploadPostViewModel.swift
+++ Instagram_Tutorial/Core/UploadPost/UploadPostViewModel.swift
@@ -27,6 +27,8 @@ class UploadPostViewModel: ObservableObject {
         self.recentPrompts = [
             "Generate a stunning concept for an expansive fantasy world, complete with enchanted forests, towering castles, and mythical creatures, setting the stage for a grand adventure.",
             "Generate a cartoon character in the style of classic Disney animation, with large, expressive eyes and whimsical features.",
+            "Cityscape painting during a rainy day, focusing on reflections in puddles with a mix of soft and harsh brush strokes.",
+            "Surreal painting of a child dreaming about floating among stars, using soft, dreamy colors and elements of fantasy.",
             "Generate poetic images of nature, blending landscapes and elements to convey the beauty and harmony of the natural world for nature-centric blogs/environmental causes.",
             "Create stunning photos capturing the essence of a high-fashion runway show, featuring the latest couture."
         ].map({ content in
