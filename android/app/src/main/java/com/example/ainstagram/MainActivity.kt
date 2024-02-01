2bedf654ab02ac57fb4b099c6d86de5773f83266
BumgeunSong <isp1195@gmail.com>
2024-02-01 11:03:03 +0900

    feat: 텍스트 배경색 바꾸기

--- android/app/src/main/java/com/example/ainstagram/MainActivity.kt
+++ android/app/src/main/java/com/example/ainstagram/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ainstagram.ui.theme.AInstagramTheme
 
@@ -28,10 +29,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    Surface(color = Color.LightGray) {
+        Text(
             text = "제 이름은 ($name)입니다!",
             modifier = modifier
-    )
+        )
+    }
 }
 
 @Preview(showBackground = true)
