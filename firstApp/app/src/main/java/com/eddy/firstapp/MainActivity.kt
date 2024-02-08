542b6a5e78197deb1962ea26022b45e25489d6da
BumgeunSong <isp1195@gmail.com>
2024-02-08 22:19:28 +0900

    이미지 클릭 이벤트 받아와서 토스트 띄우기

--- firstApp/app/src/main/java/com/eddy/firstapp/MainActivity.kt
+++ firstApp/app/src/main/java/com/eddy/firstapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.eddy.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val profile1 = findViewById<ImageView>(R.id.profile_1)
+        profile1.setOnClickListener {
+            Toast.makeText(this, "1번이 클릭되었음!", Toast.LENGTH_LONG).show()
+        }
     }
 }
