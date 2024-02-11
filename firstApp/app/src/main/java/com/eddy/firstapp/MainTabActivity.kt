273d8954818277088306092fbdd8ae7543ee1aa4
BumgeunSong <isp1195@gmail.com>
2024-02-11 14:16:48 +0900

    탭 내비게이션을 적용할 새로운 메인탭 액티비티 추가

---
+++ firstApp/app/src/main/java/com/eddy/firstapp/MainTabActivity.kt
@@ -0,0 +1,13 @@
+package com.eddy.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eddy.firstapp.R.layout.activity_main_tab
+
+class MainTabActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_main_tab)
+
+    }
+}
