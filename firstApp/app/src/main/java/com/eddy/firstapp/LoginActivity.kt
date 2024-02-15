2bfac374c9f3447d238e9f405b4dacb09bd33bcf
BumgeunSong <isp1195@gmail.com>
2024-02-15 11:10:23 +0900

    회원가입 눌렀을 때 firebase로 가입 요청 보내기

--- firstApp/app/src/main/java/com/eddy/firstapp/LoginActivity.kt
+++ firstApp/app/src/main/java/com/eddy/firstapp/LoginActivity.kt
@@ -3,8 +3,10 @@ package com.eddy.firstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.eddy.firstapp.databinding.ActivityDetailBinding
 import com.eddy.firstapp.databinding.ActivityLoginBinding
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 
@@ -17,9 +19,28 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
         auth = Firebase.auth
 
-        binding.loginButton.setOnClickListener {
-            Log.d("LoginActivity", binding.emailField.text.toString())
-            Log.d("LoginActivity", binding.passwordField.text.toString())
+        binding.signInButton.setOnClickListener {
+            val email = binding.emailField.text.toString()
+            val password = binding.passwordField.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("LoginActivity", "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        // updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("LoginActivity", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        // updateUI(null)
+                    }
+                }
         }
     }
 }
